3dfb15135908ef2b8cdbd2a3dde1e3c84c156995
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2022-10-04 01:23:24 +0300

    first commit just uploading image without set name or id

---
+++ server.js
@@ -0,0 +1,52 @@
+require("dotenv").config()
+const express=require('express')
+const PORT=process.env.PORT 
+const { S3Client } = require('@aws-sdk/client-s3')
+const multer=require('multer')
+const multerS3=require('multer-s3')
+
+
+const app=express()
+
+
+
+const s3=new S3Client({
+    region: process.env.S3_BUCKET_REGION,
+    credentials:{
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    }
+    
+    
+})
+const BUCKET=process.env.S3_BUCKET_NAME
+
+const upload=multer({
+    storage:multerS3({
+        bucket:BUCKET,
+        s3:s3,
+        
+        metadata: function (req, file, cb) {
+            cb(null, {
+              fieldName: file.fieldname
+            });
+          },
+        key:(req,file,cb)=>{
+            cb(null,file.originalname);
+        }
+    })
+})
+
+app.post('/upload',upload.single("image"),(req,res)=>{
+    console.log(req.file) 
+})
+ 
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server started on port ${PORT}`)
+})
